682f5523fb56dfd26a892c831307d564f0f56d85
BranceA <brancealexander@gmail.com>
2019-08-20 16:26:19 -0500

    added some more text displays

--- pod-game.js
+++ pod-game.js
@@ -5,6 +5,7 @@ var commandsFooter = document.getElementById('commands-box');
 var submitButton = document.getElementById('submit');
 var submittedText = document.getElementById('text-input');
 var inventoryAside = document.getElementById('inventory-aside');
+var didPlayerPullLever = false;
 
 
 commandsFooter.innerHTML = availableActions.join(" ");
@@ -29,10 +30,18 @@ function youGotAnItem(itemName, itemAction) {
 function checkKeyWords() {
     var checkThisText = submittedText.value;
     if (checkThisText.toLowerCase().indexOf('use') !== -1 && checkThisText.toLowerCase().indexOf('key') !== -1) {
-    bigText.innerText = 'Ignore this.';
+        bigText.innerHTML = 'Ignore this.';
     } else if (checkThisText.toLowerCase().indexOf('get') !== -1 && checkThisText.toLowerCase().indexOf('key') !== -1){
         youGotAnItem("Key", "Unlock");
-    } else if (checkThisText.toLowerCase().indexOf('get') !== -1 && checkThisText.toLowerCase().indexOf('key')) {
-
+        bigText.innerHTML = 'You pick up the key.';
+    } else if (checkThisText.toLowerCase().indexOf('get') !== -1 && checkThisText.toLowerCase().indexOf('key') !== -1) {
+        bigText.innerHTML = 'You pick up the key.';
+    } else if (checkThisText.toLowerCase().indexOf('look') !== -1 && checkThisText.toLowerCase().indexOf('bookcase') !== -1) {
+        bigText.innerHTML = 'This thing is kinda gross. The titles are all faded off the spines of the books and when you open one the pages crumble to dust. Wait is that a <em>lever</em>?';
+    } else if (checkThisText.toLowerCase().indexOf('look') !== -1 && checkThisText.toLowerCase().indexOf('lever') !== -1) {
+        bigText.innerHTML = 'You take a gander at it. It is a wooden lever that probably does something if you <em>use</em> it.';
+    } else if (checkThisText.toLowerCase().indexOf('use') !== -1 && checkThisText.toLowerCase().indexOf('lever') !== -1) {
+        bigText.innerHTML = 'There is a little resistance but you pull the lever. A small panel pops open in the back of the <em>bookcase</em>. Inside you see a <em>key</em>.';
+        didPlayerPullLever = true;
     }
 }

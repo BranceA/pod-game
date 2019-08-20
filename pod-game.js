4ee69eab7aa18bd7d29e2dfe4f08287a5457b74c
BranceA <brancealexander@gmail.com>
2019-08-20 16:29:15 -0500

    player now has to pull the lever to get the key

--- pod-game.js
+++ pod-game.js
@@ -31,11 +31,8 @@ function checkKeyWords() {
     var checkThisText = submittedText.value;
     if (checkThisText.toLowerCase().indexOf('use') !== -1 && checkThisText.toLowerCase().indexOf('key') !== -1) {
         bigText.innerHTML = 'Ignore this.';
-    } else if (checkThisText.toLowerCase().indexOf('get') !== -1 && checkThisText.toLowerCase().indexOf('key') !== -1){
-        youGotAnItem("Key", "Unlock");
-        bigText.innerHTML = 'You pick up the key.';
-    } else if (checkThisText.toLowerCase().indexOf('get') !== -1 && checkThisText.toLowerCase().indexOf('key') !== -1) {
-        bigText.innerHTML = 'You pick up the key.';
+    } else if (checkThisText.toLowerCase().indexOf('get') !== -1 && checkThisText.toLowerCase().indexOf('key') !== -1 && didPlayerPullLever === false) {
+        bigText.innerHTML = 'What key?';
     } else if (checkThisText.toLowerCase().indexOf('look') !== -1 && checkThisText.toLowerCase().indexOf('bookcase') !== -1) {
         bigText.innerHTML = 'This thing is kinda gross. The titles are all faded off the spines of the books and when you open one the pages crumble to dust. Wait is that a <em>lever</em>?';
     } else if (checkThisText.toLowerCase().indexOf('look') !== -1 && checkThisText.toLowerCase().indexOf('lever') !== -1) {
@@ -43,5 +40,8 @@ function checkKeyWords() {
     } else if (checkThisText.toLowerCase().indexOf('use') !== -1 && checkThisText.toLowerCase().indexOf('lever') !== -1) {
         bigText.innerHTML = 'There is a little resistance but you pull the lever. A small panel pops open in the back of the <em>bookcase</em>. Inside you see a <em>key</em>.';
         didPlayerPullLever = true;
+    } else if (checkThisText.toLowerCase().indexOf('get') !== -1 && checkThisText.toLowerCase().indexOf('key') !== -1 && didPlayerPullLever === true){
+        youGotAnItem("Key", "Unlock");
+        bigText.innerHTML = 'You pick up the key.';
     }
 }
